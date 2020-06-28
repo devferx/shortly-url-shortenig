53493989ae83776d9ccd51954684c0057e3ab416
devferx <ferkki@hotmail.com>
2020-06-28 00:04:05 -0400

    Updates

---
+++ precache-manifest.2529bd26c805a6ca2f3be3cca0e816ed.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86627d800609048ea46df584a41e7d62",
+    "url": "/shortly-url-shortening/index.html"
+  },
+  {
+    "revision": "56e2bd83d74e5d30885f",
+    "url": "/shortly-url-shortening/static/js/2.d31f6bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shortly-url-shortening/static/js/2.d31f6bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17557fd9fe7dde1e436",
+    "url": "/shortly-url-shortening/static/js/main.f3d3d899.chunk.js"
+  },
+  {
+    "revision": "c6169e65bd67ba5b9ccb",
+    "url": "/shortly-url-shortening/static/js/runtime-main.59534ae9.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/shortly-url-shortening/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/shortly-url-shortening/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/shortly-url-shortening/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/shortly-url-shortening/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/shortly-url-shortening/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/shortly-url-shortening/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/shortly-url-shortening/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/shortly-url-shortening/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "754c5ee7b5b89a9a5f68728e337c0fdc",
+    "url": "/shortly-url-shortening/static/media/logo-white.754c5ee7.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/shortly-url-shortening/static/media/logo.6387840f.svg"
+  }
+]);
